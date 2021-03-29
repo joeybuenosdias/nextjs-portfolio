00f4ea6de05cfbc0a22377e1f8f836e5be3cddfa
Joey Schrader <dev@joeyschrader.com>
2021-03-29 15:38:00 -0700

    Add dependancies for auto imports

---
+++ components/home/home.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import Image from 'next/image'
+
+export default function Home({ name, title }) {
+	return (
+		<div>
+			<h1>{name}</h1>
+			<h3>{title}</h3>
+			<Image
+				alt="Beach sunset with palm trees"
+				src="/beach.svg"
+				width={350}
+				height={350}
+			/>
+		</div>
+	)
+}
+
+Home.propTypes = {
+	name: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+}
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	collectCoverage: true,
+	collectCoverageFrom: [
+		'**/*.{js,jsx}',
+		'!**/node_modules/**',
+		'!**/.next/**',
+	],
+	setupFilesAfterEnv: ['./jest.setup.js'],
+	moduleNameMapper: {
+		'^@components(.*)$': '<rootDir>/components$1',
+	},
+}
