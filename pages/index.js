a541867afb16ad4c60e440d93432b338da01b5b0
Joey Schrader <dev@joeyschrader.com>
2021-03-29 14:55:09 -0700

    Set up proptypes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
+import PropTypes from 'prop-types'
+
+export default function Home({ name, title }) {
+	console.log('name', name)
+	console.log('title', title)
 	return (
 		<div>
 			Home Page Content
 		</div>
 	)
+}
+
+Home.propTypes = {
+	name: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+}
+
+export async function getServerSideProps() {
+	const name = 'Joey Schrader'
+	const title = 'Front End Engineer'
+
+	return {
+		props: {
+			name,
+			title,
+		},
+	}
 }
