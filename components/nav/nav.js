046ea5282ba2cfd9bae18b37451d136c2c0e7373
Joey Schrader <dev@joeyschrader.com>
2021-03-29 16:40:31 -0700

    Add basic navigation

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,7 +1,29 @@
+import Link from 'next/link'
+
+const navData = [
+	{
+		href: '/',
+		name: 'Home',
+	},
+	{
+		href: '/resume',
+		name: 'Resume',
+	},
+	{
+		href: '/contact',
+		name: 'Contact',
+	},
+]
+
 export default function Nav() {
 	return (
 		<nav>
-			I am nav
+			{navData.map((navLink) => (
+				<Link href={navLink.href}>
+					{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+					<a>{navLink.name}</a>
+				</Link>
+			))}
 		</nav>
 	)
 }
