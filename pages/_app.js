be27cfe5a17ac63ceca8255494784006d4c59c1e
Joey Schrader <dev@joeyschrader.com>
2021-03-30 05:52:17 -0700

    useDarkMode hook working

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types'
+import { ThemeProvider } from 'styled-components'
+import useDarkMode from 'use-dark-mode'
 import { Nav } from '@components'
 import { darkMode, lightMode } from '@themes'
 
 export default function App({ Component, pageProps }) {
+	const { value: isDarkMode } = useDarkMode()
+	const activeTheme = isDarkMode ? darkMode : lightMode
+
+	console.log('isDarkMode', isDarkMode)
 	console.log('darkMode', darkMode)
 	console.log('lightMode', lightMode)
 	return (
-		<div>
+		<ThemeProvider theme={activeTheme}>
 			<Component {...pageProps} />
 			<Nav />
-		</div>
+		</ThemeProvider>
 	)
 }
 
