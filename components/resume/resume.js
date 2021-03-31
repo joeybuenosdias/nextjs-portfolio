6f9ae61d7488744e816744dd975df1d04026f5e0
Joey Schrader <dev@joeyschrader.com>
2021-03-31 06:18:38 -0700

    Create basic section component

--- components/resume/resume.js
+++ components/resume/resume.js
@@ -1,7 +1,29 @@
+import PropTypes from 'prop-types'
+import { StyledSection } from './resume.styled'
+
 export default function Resume() {
 	return (
 		<div>
-			Resume
+			<Section title="SUMMARY">
+				<div>Summary Content here</div>
+			</Section>
 		</div>
 	)
+}
+
+function Section({
+	title,
+	children,
+}) {
+	return (
+		<StyledSection>
+			<h2>{title}</h2>
+			<div>{children}</div>
+		</StyledSection>
+	)
+}
+
+Section.propTypes = {
+	title: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired,
 }
---
+++ components/resume/resume.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledSection = styled.section`
+	padding: 16px;
+	background-color: blue;
+`
+
+export const StyledTitle = styled.h2`
+	background-color: yellow;
+`
