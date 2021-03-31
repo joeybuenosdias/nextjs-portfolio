b78cbd82994c7e012c751ab2670ca05fa3cb45e7
Joey Schrader <dev@joeyschrader.com>
2021-03-31 07:46:10 -0700

    Touch up styles

--- components/nav/nav.styled.js
+++ components/nav/nav.styled.js
@@ -13,7 +13,7 @@ export const StyledNav = styled.nav`
 	display: flex;
 	justify-content: space-around;
 	background: ${({ theme }) => theme.backgroundColor};
-	padding: 2em 0;
+	padding: 1.5em 0;
 `
 
 export const StyledLink = styled.a`
