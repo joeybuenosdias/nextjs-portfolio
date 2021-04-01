d57af9c82f1c8be1a49e7596e95013890400ef2f
Joey Schrader <dev@joeyschrader.com>
2021-04-01 07:55:52 -0700

    Complete test coverage for header

--- components/header/header.js
+++ components/header/header.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types'
+import Image from 'next/image'
 import { Toggle } from 'react-toggle-component'
-import { StyledHeader } from './header.styled'
+import {
+	StyledHeader,
+	StyledLabel,
+	StyledModeWrapper,
+} from './header.styled'
 
 export default function Header({
 	setIsDarkMode,
@@ -12,20 +17,56 @@ export default function Header({
 
 	return (
 		<StyledHeader>
-			<Toggle
-				name="theme-toggle"
-				knobColor={isDarkMode ? '#333333' : '#fff'}
-				onToggle={handleToggle}
-				checked={isDarkMode}
-				backgroundColor="#0BD3D3"
-				borderColor="#0BD3D3"
-				data-testid="toggle"
-			/>
+			{/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+			<StyledLabel htmlFor="theme-toggle">
+				<ModeIcon isDarkMode={isDarkMode} />
+				<Toggle
+					name="theme-toggle"
+					knobColor={isDarkMode ? '#333333' : '#fff'}
+					onToggle={handleToggle}
+					checked={isDarkMode}
+					backgroundColor="#0BD3D3"
+					borderColor="#0BD3D3"
+					data-testid="toggle"
+				/>
+			</StyledLabel>
 		</StyledHeader>
 	)
 }
 
 Header.propTypes = {
 	setIsDarkMode: PropTypes.func.isRequired,
+	isDarkMode: PropTypes.bool.isRequired,
+}
+
+function ModeIcon({ isDarkMode }) {
+	if (isDarkMode) {
+		return (
+			<StyledModeWrapper>
+				<Image
+					alt="Moon and stars"
+					src="/moon.svg"
+					width={12}
+					height={12}
+					data-testid="moon"
+				/>
+			</StyledModeWrapper>
+		)
+	}
+
+	return (
+		<StyledModeWrapper>
+			<Image
+				alt="Sun"
+				src="/sun.svg"
+				width={14}
+				height={14}
+				data-testid="sun"
+			/>
+		</StyledModeWrapper>
+	)
+}
+
+ModeIcon.propTypes = {
 	isDarkMode: PropTypes.bool.isRequired,
 }
